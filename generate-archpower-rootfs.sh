1d9364ba0dc74cfe8779ba9267e0bb210ff690e0
techflashYT <officialTechflashYT@gmail.com>
2024-10-23 05:15:24 +0000

    add ArchPOWER rootfs generation

---
+++ generate-archpower-rootfs.sh
@@ -0,0 +1,82 @@
+#!/bin/sh -e
+
+usage() {
+	cat << EOF
+Usage: generate-archpower-rootfs.sh [output dir]
+
+Options:
+	--help:               Display this help text.
+
+This script generates an ArchPOWER rootfs with Wii Linux packages installed.
+It expects that you are using packages.wii-linux.org as the repo.  Should this
+not be the case, you can modify conf/pacman.conf to point it at your custom
+repo.
+
+Note, this script **WILL** rm -rf whatever directory is passed.  Please
+be careful!  No issues will be accepted about
+"I hosed my system because I passed '/' as the first argument"!!!!!!
+
+Report any bugs to the GitHub issues page.
+EOF
+}
+
+OUT="$PWD/$1"
+
+# strip trailing /
+OUT="${OUT%/}"
+
+if [ -f ./utils.sh ]; then . ./utils.sh; cd ../; BASE="$PWD"; cd - > /dev/null
+elif [ -f ./build-stack/utils.sh ]; then . ./build-stack/utils.sh; BASE="$PWD"
+elif [ -f ../build-stack/utils.sh ]; then . ../build-stack/utils.sh; cd ../; BASE="$PWD"; cd - > /dev/null
+else
+	echo "failed to find utils.sh" >&2
+	exit 1
+fi
+
+
+if [ "$1" = "/" ]; then
+	fatal "Prevented hosing host system"
+fi
+
+for arg in "$@"; do
+	case "$arg" in
+		"") usage; exit 1 ;; # user didn't provide anything
+		-h|--help) usage; exit 0 ;; # show help
+		--) break ;;
+		-*) error "bad argument"; usage; exit 1 ;;
+		*) break ;; # probably a directory
+	esac
+done
+
+if [ "$(id -u)" != "0" ]; then
+	fatal "Script must be run as root, as it uses pacstrap."
+fi
+
+echo "output dir: $OUT"
+echo "base dir: $BASE"
+
+cd "$BASE"
+rm -rf "$OUT"
+mkdir -p "$OUT"
+
+pacstrap -KMC build-stack/conf/wiilinux-pacman.conf "$OUT" base wii-linux-kernel-stable wii-linux-loader-stable wii-linux-meta gumboot-utils networkmanager vim nano
+
+# pacstrap doesn't maintain our custom pacman.conf
+cp build-stack/conf/wiilinux-pacman.conf "$OUT/etc/pacman.conf"
+cat << EOF > "$OUT"/setup.sh
+#!/bin/sh
+pacman-key --init
+pacman-key --populate archpower
+echo 'root:wiilinux' | chpasswd
+gumboot-mkconfig -o /boot/gumboot/gumboot.lst
+systemctl enable NetworkManager
+systemctl enable systemd-timesyncd
+
+EOF
+chmod +x "$OUT"/setup.sh
+chroot "$OUT" /setup.sh
+
+echo "making a tarball of the rootfs"
+tar --preserve-permissions --acls --xattrs --sparse -czf "${OUT}-boot.tar.gz" -C "$OUT/boot" .
+rm -rf "$OUT"/boot/*
+tar --preserve-permissions --acls --xattrs --sparse -czf "${OUT}-root.tar.gz" -C "$OUT/" .
