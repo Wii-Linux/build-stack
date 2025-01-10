01ffb511adac4773ab2e045f98a2df1c32c32841
techflashYT <officialTechflashYT@gmail.com>
2025-01-09 21:49:05 -0700

    c.sh: -Ofast to -O2

--- c.sh
+++ c.sh
@@ -2,7 +2,7 @@
 
 powerpc-unknown-linux-gnu-gcc -Wall -Wextra -std=gnu2x \
 	-nostdlib -nostartfiles -nostdinc \
-	-Ofast -DPROD_BUILD \
+	-O2 -DPROD_BUILD \
 	-Wl,-rpath ../loader-img-src/usr/lib -L ../loader-img-src/usr/lib \
 	-isystem ../dummy-sysroot-src/powerpc-buildroot-linux-uclibc/sysroot/usr/include/ \
 	-isystem ../dummy-sysroot-src/lib/gcc/powerpc-buildroot-linux-uclibc/13.3.0/include \
