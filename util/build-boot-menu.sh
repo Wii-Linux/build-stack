b444dc34ccfbc4f1cd590b33e1323903deda49cd
techflashYT <officialTechflashYT@gmail.com>
2025-02-26 18:51:27 -0700

    build-boot-menu: updates to build the new style

--- util/build-boot-menu.sh
+++ util/build-boot-menu.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
 # args are used for -DPROD_BUILD, -DDEBUG_WII, or -DDEBUG_PC
-powerpc-unknown-linux-gnu-gcc -Wall -Wextra -std=gnu2x \
+powerpc-unknown-linux-gnu-gcc -Wall -Wextra -Wno-stringop-truncation -std=gnu2x \
 	-nostdlib -nostartfiles -nostdinc \
-	-O2 "$@" \
+	-O2 -flto "$@" \
 	-Wl,-rpath ../loader-img-src/usr/lib -L ../loader-img-src/usr/lib \
 	-isystem ../dummy-sysroot-src/powerpc-buildroot-linux-uclibc/sysroot/usr/include/ \
 	-isystem ../dummy-sysroot-src/lib/gcc/powerpc-buildroot-linux-uclibc/13.3.0/include \
 	-l:libc.so.1 -lblkid -e main \
-	../boot-stack/loader-img-full/bootMenu.c -o ../loader-img-src/bin/boot_menu
+	../boot-stack/loader-img-full/*.c -o ../loader-img-src/bin/boot_menu
