d1a3098d010cb08c60fb5f6274f8eb17588d47d6
techflashYT <officialTechflashYT@gmail.com>
2025-08-09 16:01:00 -0700

    build-boot-menu: update to newer GCC path
    god I really need to make this clearer

--- util/build-boot-menu.sh
+++ util/build-boot-menu.sh
@@ -6,6 +6,6 @@ powerpc-unknown-linux-gnu-gcc -Wall -Wextra -Wno-stringop-truncation -std=gnu2x 
 	-O2 -flto "$@" \
 	-Wl,-rpath ../loader-img-src/usr/lib -L ../loader-img-src/usr/lib \
 	-isystem ../dummy-sysroot-src/powerpc-buildroot-linux-uclibc/sysroot/usr/include/ \
-	-isystem ../dummy-sysroot-src/lib/gcc/powerpc-buildroot-linux-uclibc/13.3.0/include \
+	-isystem ../dummy-sysroot-src/lib/gcc/powerpc-buildroot-linux-uclibc/13.4.0/include \
 	-l:libc.so.1 -lblkid -e main \
 	../boot-stack/loader-img-full/*.c -o ../loader-img-src/bin/boot_menu
